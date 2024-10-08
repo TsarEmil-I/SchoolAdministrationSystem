2119de0758245f4cab17cbd18dc5714f2ee48761
eanes <eanes@EMIL-ANESTIEV>
2024-10-08 21:14:23 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SchoolAdministrationSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
