adb42b4df560a909e86765d3f98bef297b21acb2
TsarEmil <eanestiev58@gmail.com>
2024-10-20 17:05:26 +0300

    fix SQL error

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -12,7 +12,7 @@ namespace SchoolAdministrationSystem.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=SchoolAdministrationDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SchoolAdministrationDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
             base.OnConfiguring(optionsBuilder);
         }
 
