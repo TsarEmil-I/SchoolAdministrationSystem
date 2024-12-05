5c6eaa1dcad9aaf83aff87b8a7b62134d2e077f9
TsarEmil <eanestiev58@gmail.com>
2024-12-05 12:40:44 +0200

    allow dash ("-") in names

--- Data/Student.cs
+++ Data/Student.cs
@@ -8,17 +8,18 @@ namespace SchoolAdministrationSystem.Data
     {
         [Required]
         [DisplayName("First Name")]
-        [RegularExpression(@"^[А-Яа-яЁё]+$", ErrorMessage = "First Name must contain only Cyrillic letters.")]
+        [RegularExpression(@"^[А-Яа-яЁё-]+$", ErrorMessage = "First Name must contain only Cyrillic letters and dashes.")]
         public string FirstName { get; set; }
 
         [DisplayName("Second Name")]
-        [RegularExpression(@"^[А-Яа-яЁё]+$", ErrorMessage = "Middle Name must contain only Cyrillic letters.")]
+        [RegularExpression(@"^[А-Яа-яЁё-]+$", ErrorMessage = "Middle Name must contain only Cyrillic letters and dashes.")]
         public string MiddleName { get; set; }
 
         [Required]
         [DisplayName("Last Name")]
-        [RegularExpression(@"^[А-Яа-яЁё]+$", ErrorMessage = "Last Name must contain only Cyrillic letters.")]
+        [RegularExpression(@"^[А-Яа-яЁё-]+$", ErrorMessage = "Last Name must contain only Cyrillic letters and dashes.")]
         public string LastName { get; set; }
+
 
         public string FullName
         {
---
+++ Migrations/20241205103828_allowNameDashes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolAdministrationSystem.Migrations
+{
+    /// <inheritdoc />
+    public partial class allowNameDashes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
